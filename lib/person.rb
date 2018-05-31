1be80a09c50746551c1e148d8afb212bc4bebb4e
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-31 08:13:16 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def initjob(job) 
+    @job = job
+  end
+  
+  def job 
+    @job
+  end
+  
+  def job=(job)
+    new_job = job 
+    @job = new_job
+  end
+  
+  
+  
+  
+end
